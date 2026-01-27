ab88c4d808f7435be3866e6be72a0f252e0ac425
soeren <mail@floatingbitsmedia.de>
2026-01-27 11:53:55 +0100

    Current app configuration

--- app/config/app.py
+++ app/config/app.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 
 from resonator_ml.machine_learning.custom_loss_functions import relative_l1_with_penalty
 from resonator_ml.machine_learning.loop_filter.neural_network import NeuralNetworkParameters, DelayPattern
-from resonator_ml.machine_learning.training import TrainingParameters
+from resonator_ml.machine_learning.training.parameters import TrainingParameters
 
 
 @dataclass(frozen=True)
@@ -20,12 +20,12 @@ class Config:
     sample_rate: int = 44100
     base_frequency: float = 82.46
     output_soundfile_length: float = 5
-    training_parameters: TrainingParameters = field(default_factory=lambda : TrainingParameters(batch_size=32, epochs=100, learning_rate=8e-6,
-                                          loss_function=relative_l1_with_penalty, max_training_data_frames=20000))
+    training_parameters: TrainingParameters = field(default_factory=lambda : TrainingParameters(batch_size=128, epochs=500, learning_rate=3e-4,
+                                          loss_function=relative_l1_with_penalty, max_training_data_frames=400000))
     neural_network_parameters: NeuralNetworkParameters = field(default_factory=lambda : NeuralNetworkParameters(
-        num_hidden_per_layer=20,
+        num_hidden_per_layer=1500,
         num_hidden_layers=1,
-        delay_patterns=[DelayPattern(0, 0, 2), DelayPattern(1, 3, 2),DelayPattern(0.25, 2, 2),DelayPattern(0.75, 2, 2),DelayPattern(0.5, 2, 2)]
+        delay_patterns=[DelayPattern(1, 3, 2),DelayPattern(0.25, 2, 2),DelayPattern(0.75, 2, 2),DelayPattern(0.5, 2, 2)]
     ))
 
 
