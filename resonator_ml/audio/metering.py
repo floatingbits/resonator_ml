2f3289c235772eb1c8f789b2a245d1e7caf31990
soeren <mail@floatingbitsmedia.de>
2026-02-23 20:49:03 +0100

    adding scypi and pytest

--- resonator_ml/audio/metering.py
+++ resonator_ml/audio/metering.py
@@ -3,24 +3,24 @@ from .core import MonoProcessor, MonoFrame
 import numpy as np
 import math
 from .delay_lines import SampleAccurateDelayLineMono
-
+from scipy.signal import stft
 
 class StatefulEnergyMeter(MonoProcessor):
     def __init__(self, window_size: int, sample_rate: int):
         super().__init__(sample_rate)
         self.window_size = window_size
-        self.buffer = np.zeros(window_size, dtype=np.float64)
+        self.buffer = np.zeros(window_size, dtype=np.float32)
         self.buffer_pos = 0
         self.energy = 0.0
         self.filled = 0  # wie viele Samples schon im Buffer sind
 
     def prepare(self):
-        self.buffer = np.zeros(self.window_size, dtype=np.float64)
+        self.buffer = np.zeros(self.window_size, dtype=np.float32)
         self.energy = 0.0
         self.filled = 0
 
     def process_mono(self, samples: MonoFrame) -> MonoFrame:
-        samples = np.asarray(samples, dtype=np.float64)
+        samples = np.asarray(samples, dtype=np.float32)
         output = np.empty_like(samples)
 
         for i, x in enumerate(samples):
@@ -72,4 +72,19 @@ class DecayMeter(MonoProcessor):
                     output[i] = -1*math.log(delayed_energy/cur_energy, 10)
 
         return output
+
+
+def compute_spectrogram(x, fs, n_fft=1024, hop=256):
+    f, t, zxx = stft(
+        x,
+        fs=fs,
+        nperseg=n_fft,
+        noverlap=n_fft - hop,
+        window="hann",
+        padded=False,
+        boundary=None,
+    )
+
+    magnitude_db = 20 * np.log10(np.abs(zxx) + 1e-12)
+    return f, t, magnitude_db
 
